9be2ff1c47717a1a60d391efeb8a70ca0da13804
wlqoh <wylqoh@gmail.com>
2025-01-05 08:06:24 +0300

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Мурад on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
