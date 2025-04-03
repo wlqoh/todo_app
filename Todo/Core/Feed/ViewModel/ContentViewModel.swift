ddd7ec3ca59628c3662fcc8f1de9d055919e8454
wlqoh <wylqoh@gmail.com>
2025-04-03 13:08:32 +0300

    some refactor

---
+++ Todo/Core/Feed/ViewModel/ContentViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ContentViewModel.swift
+//  Todo
+//
+//  Created by Мурад on 3/4/25.
+//
+
+import SwiftUI
+
+struct ContentViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentViewModel()
+}
